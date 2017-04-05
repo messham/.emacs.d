66718e41d66118ab6c206863fa9cca880a73bc89
jm <jm.aandkrobotics@gmail.com>
2017-04-04 17:34:21 -0700

    Initial commit -- terminal-mode .emacs.d is a lightweight branch w many slow loading features removed

--- init.el
+++ init.el
@@ -7,35 +7,28 @@
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 
-;; emacs backup settings
-(setq backup-directory-alist `((".*" . "~/.saves"))
-      backup-by-copying t
-      delete-old-versions t
-      version-control t)
-
 ;; add file path to non-package programs
 (add-to-list 'load-path "~/.emacs.d/lisp/")
 
 ;; load package.el and use-package.el
+;; TERMINAL-MODE: may need to uncomment all package stuff on first startup
 (require 'package)
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
-(add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/") t)
+;; (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+;; (add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/") t)
 (setq package-enable-at-startup nil)
 (package-initialize)
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
+;; (unless (package-installed-p 'use-package)
+;;   (package-refresh-contents)
+;;   (package-install 'use-package))
 (require 'use-package)
+
+;; allow copy with mouse in emacs terminal mode
+(setq x-select-enable-clipboard t)
+
+;; indent with spaces instead of tabs
+(setq-default indent-tabs-mode nil)
 
 ;; set individual package configs
 (setq load-prefer-newer t)
 (require 'use-package-configs)
-
-;; load workspace specific settings
-(when (eq system-type 'darwin)
-  (require 'macbook-settings))
-
-;; misc preferences
-(add-to-list 'auto-mode-alist '("\\.m\\'" . octave-mode))
-
 
--- lisp/use-package-configs.el
+++ lisp/use-package-configs.el
@@ -10,40 +10,29 @@
     (add-to-list 'ac-modes 'c++-mode)))
 ;; TODO: configure menu-map for using tab as completion only (not cycle)
 
-(use-package color-theme-sanityinc-tomorrow
-  :ensure t
-  :init
-  (progn
-    (load-theme 'sanityinc-tomorrow-night :no-confirm)
-    (setf frame-background-mode 'dark)
-    (global-hl-line-mode 1)
-    (show-paren-mode 1)
-    (custom-set-faces
-     '(cursor               ((t :background "#eebb28")))
-     '(linum                ((t :foreground "gray55")))
-     '(diff-added           ((t :foreground "green" :underline nil)))
-     '(diff-removed         ((t :foreground "red" :underline nil)))
-     '(highlight            ((t :background "black" :underline nil)))
-     '(magit-item-highlight ((t :background "black")))
-     '(hl-line              ((t :background "gray10"))))
-    (add-hook 'c++-mode-hook #'linum-mode)))
+;; (use-package color-theme-sanityinc-tomorrow
+;;   :ensure t
+;;   :init
+;;   (progn
+;;     (load-theme 'sanityinc-tomorrow-night :no-confirm)
+;;     (setf frame-background-mode 'dark)
+;;     (global-hl-line-mode 1)
+;;     (show-paren-mode 1)
+;;     (custom-set-faces
+;;      '(cursor               ((t :background "#eebb28")))
+;;      '(linum                ((t :foreground "gray55")))
+;;      '(diff-added           ((t :foreground "green" :underline nil)))
+;;      '(diff-removed         ((t :foreground "red" :underline nil)))
+;;      '(highlight            ((t :background "black" :underline nil)))
+;;      '(magit-item-highlight ((t :background "black")))
+;;      '(hl-line              ((t :background "gray10"))))
+;;     (add-hook 'c++-mode-hook #'linum-mode)))
 
-(use-package dired
-  :config
-  (define-key dired-mode-map (kbd "C-l") #'dired-up-directory))
-
-(use-package exec-path-from-shell
-  :if (eq system-type 'darwin)
-  :ensure t
-  :config
-  (exec-path-from-shell-initialize))
-
-(use-package expand-region
-  :ensure t
-  :bind ("C-=" . er/expand-region))
-
-(use-package haskell-mode
-  :ensure t)
+;; TERMINAL-MODE: ctrl and any num row keys doesn't appear to work in terminal
+;;                TODO: enable expand-region and set to non num row key
+;; (use-package expand-region
+;;   :ensure t
+;;   :bind ("C-=" . er/expand-region))
 
 (use-package helm
   :ensure t
@@ -80,33 +69,9 @@
     (setf helm-swoop-split-with-multiple-windows t)
     (global-set-key (kbd "C-s") #'helm-swoop)))
 
-(use-package magit
-  :ensure t
-  :bind ("C-x g" . magit-status))
-
-(use-package org
-  :ensure t
-  :config
-  (progn
-    (require 'org-agenda-add-overlays)            ;; agenda + capture settings
-    (add-hook 'org-agenda-finalize-hook 'org-agenda-add-overlays)
-    (setf org-directory "~/org")
-    (setf org-agenda-files (list org-directory))
-    (setf org-default-notes-file (concat org-directory "/notes.org"))
-    (setf org-agenda-start-on-weekday nil)
-    (setf org-log-done t)
-    (setf org-reverse-note-order t)
-    (setq org-capture-templates
-	  '(("t" "Todo" entry (file+headline (concat org-directory "/todo.org") "Tasks")
-	    "* TODO %?\n  %u")
-	   ))
-    (global-set-key (kbd "C-c c") #'org-capture)
-    (global-set-key (kbd "C-c a") #'org-agenda)
-    ;;(org-agenda-list)
-    (if (file-exists-p "~/Dropbox/Apps/MobileOrg")  ;; MobileOrg settings
-    	(progn
-    	  (setq org-mobile-directory "~/Dropbox/Apps/MobileOrg")
-    	  (setq org-mobile-inbox-for-pull "~/org/flagged.org"))))) 
+;; (use-package magit
+;;   :ensure t
+;;   :bind ("C-x g" . magit-status))
 
  (use-package smart-compile
    :ensure t
@@ -114,20 +79,13 @@
    (progn
      (global-set-key (kbd "C-c C") #'smart-compile)))
 
-(use-package virtualenvwrapper
-  :ensure t
-  :config
-  (progn
-    (venv-initialize-eshell)
-    (setf venv-location "~/.virtualenvs")))
-
-(use-package yasnippet
-  :ensure t
-  :config
-  (progn
-    (setf yas-snippet-dirs (list "~/.emacs.d/lisp/snippets" yas-installed-snippets-dir))
-    (yas-reload-all)
-    (add-hook 'c++-mode-hook #'yas-minor-mode)))
+;; (use-package yasnippet
+;;   :ensure t
+;;   :config
+;;   (progn
+;;     (setf yas-snippet-dirs (list "~/.emacs.d/lisp/snippets" yas-installed-snippets-dir))
+;;     (yas-reload-all)
+;;     (add-hook 'c++-mode-hook #'yas-minor-mode)))
 
 (provide 'use-package-configs)
 
